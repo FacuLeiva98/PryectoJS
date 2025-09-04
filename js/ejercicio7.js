433e540567b18fdf150f0ee1d914b054de83c677
facundo <leivafacundo74@gmail.com>
2025-09-04 09:53:32 -0300

    se arreglaron textos en el ejercicio7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,7 +1,7 @@
 let num1 = parseFloat(prompt("Ingresa el primer número:"));
 let num2 = parseFloat(prompt("Ingresa el segundo número:"));
-let num3 = parseFloat(prompt("Ingresa el segundo número:"));
+let num3 = parseFloat(prompt("Ingresa el tercer  número:"));
 
-const mayor =(num1,num2,num3)
+let mayor =(num1,num2,num3)
 
 document.writeln('el numero mayor es' + mayor)
